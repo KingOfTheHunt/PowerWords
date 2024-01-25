2f124e4d292ea5835c521e2f94832f537c34a507
Davi <davifrancisco214@outlook.com>
2024-01-25 10:28:29 -0300

    Creating a custom exception

---
+++ PowerWords.API/Exceptions/WordNotFoundExcption.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerWords.API.Exceptions
+{
+    public class WordNotFoundExcption : ApplicationException
+    {
+        public WordNotFoundExcption(string? message) : base(message)
+        {
+        }
+    }
+}
